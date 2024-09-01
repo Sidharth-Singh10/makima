6bd9d20ac758ceb2f89ec9f0910e427c27a58d7b
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-09-02 01:54:24 +0530

    added_operators

--- main.c
+++ main.c
@@ -8,13 +8,15 @@
 static char buffer[2048];
 
 /* Fake readline function */
-char *readline(char *prompt) {
+char *readline(char *prompt)
+{
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
   size_t len = strlen(buffer);
   char *cpy = malloc(len + 1);
   strcpy(cpy, buffer);
-  if (len > 0 && cpy[len - 1] == '\n') {
+  if (len > 0 && cpy[len - 1] == '\n')
+  {
     cpy[len - 1] = '\0'; // Remove newline character if present
   }
   return cpy;
@@ -33,7 +35,92 @@ mpc_parser_t *Operator;
 mpc_parser_t *Expr;
 mpc_parser_t *maki;
 
-int main(int argc, char *argv[]) {
+long eval_op(long x, char *op, long y)
+{
+  if (strcmp(op, "+") == 0)
+  {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0)
+  {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0)
+  {
+    return x * y;
+  }
+  if (strcmp(op, "/") == 0)
+  {
+    return x / y;
+  }
+  if (strcmp(op, "^") == 0)
+  {
+    long result = 1;
+    for (int i = 0; i < y; i++)
+    {
+      result *= x;
+    }
+    return result;
+  }
+  if (strcmp(op, "%") == 0)
+  {
+    return x % y;
+  }
+
+
+
+  return 0;
+}
+
+long eval(mpc_ast_t *t)
+{
+
+  /* If tagged as number return it directly. */
+  if (strstr(t->tag, "number"))
+  {
+    return atoi(t->contents);
+  }
+
+  /* The operator is always second child. */
+  char *op = t->children[1]->contents;
+
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+
+   if (strcmp(op, "min") == 0) {
+    long result = x;
+    while (strstr(t->children[i]->tag, "expr")) {
+      long next = eval(t->children[i]);
+      if (next < result) result = next;
+      i++;
+    }
+    return result;
+  }
+
+   if (strcmp(op, "max") == 0) {
+    long result = x;
+    while (strstr(t->children[i]->tag, "expr")) {
+      long next = eval(t->children[i]);
+      if (next > result) result = next;
+      i++;
+    }
+    return result;
+  }
+
+  while (strstr(t->children[i]->tag, "expr"))
+  {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
+int main(int argc, char *argv[])
+{
   // Create parsers
   Number = mpc_new("number");
   Operator = mpc_new("operator");
@@ -43,26 +130,33 @@ int main(int argc, char *argv[]) {
   // Define them with the following Language
   mpca_lang(MPCA_LANG_DEFAULT,
             "                                                     \
-    number   : /-?[0-9]+(\\.[0-9]+)?/ ;                              \
-    operator : '+' | '-' | '*' | '/' ;                  \
-    expr     : <number> | '(' <expr> <operator> <expr> ')' ;  \
-    maki    : /^/  <expr>+ /$/ ;             \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' | '^' | \"max\" | \"min\";  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    maki    : /^/  <operator><expr>+ /$/ ;             \
   ",
             Number, Operator, Expr, maki);
 
   puts("makima version 0.0.0.2");
   puts("Press Ctrl + C to exit ");
 
-  while (1) {
+  while (1)
+  {
     char *input = readline("makima> ");
     add_history(input);
 
     mpc_result_t r;
-    if (mpc_parse("<stdin>", input, maki, &r)) {
+    if (mpc_parse("<stdin>", input, maki, &r))
+    {
       /* On Success Print the AST */
       mpc_ast_print(r.output);
+
+      long result = eval(r.output);
+      printf("Result is %li\n", result);
       mpc_ast_delete(r.output);
-    } else {
+    }
+    else
+    {
       /* Otherwise Print the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
